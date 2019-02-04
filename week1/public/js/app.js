2f00d5114a4430d79b5b675d0760a32c367b9e49
Unknown <koenblezer@gmail.com>
2019-02-04 15:27:20 +0100

    Parsed the the response and read URL from data array

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,13 +1,15 @@
-var Http = new XMLHttpRequest();
-var url = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=JbB7Jd0sog3jHtYdb3woTkONWQYNSTfp&limit=5";
+var http = new XMLHttpRequest();
+var url = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=JbB7Jd0sog3jHtYdb3woTkONWQYNSTfp&limit=1";
 
-Http.open("GET", url);
-Http.send();
+http.open("GET", url);
+http.send();
 
-Http.onreadystatechange=function(){
+http.onreadystatechange=function(){
     if(this.readyState==4 && this.status==200){
-      var response = Http.responseText;
+      var response = http.responseText;
       console.log(response);
-      gif.innerHTML
+      var data = JSON.parse(response);
+      console.log(data);
+      document.querySelector('.gif').innerHTML = "<img src=" + data.data[0].embed_url + "\" alt=\"gif\">/";
   }
 }
