bd830c77ee8716c59a857978ec4766fa2cc6c41d
Unknown <koenblezer@gmail.com>
2019-02-04 13:17:26 +0100

    Added a test API call

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,1 +1,5 @@
-console.log("LINKED");
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=JbB7Jd0sog3jHtYdb3woTkONWQYNSTfp&limit=5");
+
+xhr.done(function(data) {
+  console.log("success got data", data);
+});
