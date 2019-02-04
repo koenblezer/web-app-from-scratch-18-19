1f518c697e4373cbac0ab1d0322866dccea3742a
Unknown <koenblezer@gmail.com>
2019-02-04 13:46:48 +0100

    Tested the API call

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,5 +1,13 @@
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=JbB7Jd0sog3jHtYdb3woTkONWQYNSTfp&limit=5");
+var Http = new XMLHttpRequest();
+var url = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=JbB7Jd0sog3jHtYdb3woTkONWQYNSTfp&limit=5";
 
-xhr.done(function(data) {
-  console.log("success got data", data);
-});
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange=function(){
+    if(this.readyState==4 && this.status==200){
+      var response = Http.responseText;
+      console.log(response);
+      gif.innerHTML
+  }
+}
